fe8662a4a1bfa0bd1740ad1377b4990cd1c33d53
cyc001 <c96138@126.com>
2020-02-21 15:48:18 +0800

    Add files via upload

---
+++ t_js/ir_temp/Draw_Canvas.ts
@@ -0,0 +1,168 @@
+﻿
+
+
+var obj_a_buf = new Array(1024)
+var amb_a_buf = new Array(1024)	
+let chart_max_len=50
+class Draw_Canvas {
+    x_scale;
+    screen_width;
+
+
+    canva: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D
+    get_x(w) {
+        return Math.floor(w * this.x_scale + 50)
+    }
+    constructor() {
+
+        screen.orientation.lock("portrait-primary");
+
+        this.screen_width = screen.width;
+        this.x_scale = (this.screen_width - 100) / chart_max_len;
+        this.canva = document.getElementById("rt_canvas") as HTMLCanvasElement;
+
+        //canvas.addEventListener('mousedown', down, false);
+        this.canva.width = this.screen_width;
+        this.canva.height = 400;
+        this.x_scale = (this.screen_width - 100) / chart_max_len;
+       // this.canva.style.zIndex = '10';
+        //this.canva.left = "50";
+
+        this.ctx = this.canva.getContext("2d");
+       // this.ctx.globalAlpha = 0.8;
+       // this.ctx.lineWidth = 1;
+       // this.ctx.lineCap = "butt";
+
+        //this.ctx.webkitImageSmoothingEnabled = false;
+        //this.ctx.imageSmoothingEnabled = false;
+        //this.ctx.oImageSmoothingEnabled = false;
+        //this.ctx.mozImageSmoothingEnabled = false;
+
+        this.ctx.imageSmoothingEnabled = true;
+        this.canva.addEventListener("mousedown", () => {
+            if (timer_status!="null") {
+                timer_status = timer_status == "pause" ? "run" : "pause";
+            }
+        });
+    }
+
+
+
+    draw_chart(start, size) {
+        //  ctx.setFillStyle('white')
+        //  ctx.fillRect(0, 0, screen_width, 350)
+        this.canva.height = this.canva.height;
+        if (size >= 100) {
+            for (var k = 0; k < 100; k++) {
+                obj_a_buf[k] = obj_list[(k + start) & 0x1ff];
+                amb_a_buf[k] = amb_list[(k + start) & 0x1ff];
+            }
+            //   this.alg_30102.maxim_heart_rate_and_oxygen_saturation(ir_a_buf, red_a_buf, 100, start);
+            // alg_30102.maxim_heart_rate_and_oxygen_saturation(app.globalData.ir_list, app.globalData.red_list, size, start);
+            //   this.alg_30102.get_30102_result(this.res_30102);
+        }
+        /*
+        rt_page.setData({
+            hr: (res_30102[1] == 1 ? res_30102[0] : " --"),
+            spo2: (res_30102[3] == 1 ? res_30102[2] : " --")
+        })
+        */
+       this.draw_line(start, size);
+        this.draw_chart_frame()
+       //////////// this.ctx..draw(false)
+    }
+    draw_line(start, size) {
+        this.ctx.beginPath()
+        this.ctx.strokeStyle = 'red';
+        //var that = this;
+        var list = obj_list;
+
+        var max = 0, min = 0xfffff;
+        var delta;
+        var k;
+        for (var i = 0; i < size; i++) {
+            k = list[(i + start) & 0x1ff];
+            if (k > max) max = k;
+            if (k < min) min = k;
+        }
+        delta = (max - min + 0.0001) * 2;
+
+        //min=min+delta/5;
+        for (var i = 0; i < size; i++) {
+
+            var x = this.get_x(i)
+            var y = 10 + (max - list[(i + start) & 0x1ff]) * 300 / delta;
+            if (i == 0) {
+                this.ctx.moveTo(x, y)
+            } else {
+                this.ctx.lineTo(x, y)
+
+            }
+
+        }
+
+        this.ctx.stroke()
+        /*
+       this.ctx.beginPath()
+       this.ctx.setStrokeStyle('blue')
+      
+       list = obj_list;
+
+       max = 0;
+       min = 0xfffff;
+
+       for (var i = 0; i < size; i++) {
+           k = list[(i + start) & 0x1ff];
+           if (k > max) max = k;
+           if (k < min) min = k;
+       }
+       delta = (max - min + 1) * 2;
+       // min = min + delta;
+
+       for (var i = 0; i < size; i++) {
+
+           var x = this.get_x(i)
+           var y = 10 + (max - list[(i + start) & 0x1ff]) * 300 / delta;
+           if (i == 0) {
+               this.ctx.moveTo(x, y)
+           } else {
+               this.ctx.lineTo(x, y)
+
+           }
+
+       }
+       
+       this.ctx.stroke()
+       */
+    }
+    draw_chart_frame() {
+
+        // <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+
+        this.ctx.beginPath()
+        this.ctx.strokeStyle ='white'
+        this.ctx.lineWidth = 1;
+        this.ctx.lineJoin = 'round';
+        this.ctx.lineCap = 'round';
+
+
+
+
+        const leftX = 50
+        const rightX = this.canva.width - 50;// parseInt(this.canva.style.width) - 50;// screen.width - 50
+
+        const topY = 10;
+        const bottomY = this.canva.height-10;
+
+        this.ctx.moveTo(leftX, topY)
+        this.ctx.lineTo(rightX, topY)
+        this.ctx.lineTo(rightX, bottomY)
+        this.ctx.lineTo(leftX, bottomY)
+        this.ctx.lineTo(leftX, topY)
+
+        this.ctx.stroke()
+
+     
+    }
+}
---
+++ t_js/ir_temp/Process.ts
@@ -0,0 +1,135 @@
+﻿
+
+
+let run_status: number = 0;
+let cpu_id: string = "";
+let dev_info_has_read = false;
+let version_info = ""
+let pid = "";
+
+let process_state = 0;
+let mcu_emissivity = 1
+
+let rt_obj = 0;
+let rt_amb = 0;
+let obj_list: Array<number> = [];
+let amb_list: Array<number> = [];
+
+class Process extends Draw_Canvas {
+    canvas: HTMLCanvasElement;
+    recv_buf: Array<number> = [];
+
+    obj_text_div: HTMLElement;
+    amb_text_div: HTMLElement;
+
+
+    sample_count: number = 0;
+
+    package_size: number = 64;
+    recv_total_pack_count: number = 0;
+    recv_data_pack_count: number = 0;
+
+    constructor() {
+        super();
+        this.obj_text_div = document.getElementById("rt_obj_div");
+        this.amb_text_div = document.getElementById("rt_amb_div");
+       // this.recv_buf = new Int8Array();
+    }
+    draw_chart_0() {
+      
+        var t_head;
+        if (this.recv_data_pack_count < 1) { return }
+
+        if (this.recv_data_pack_count < chart_max_len) {
+            this.draw_chart(0, this.sample_count);
+        }
+        else {
+            var sc = this.sample_count;
+            var sc1 = this.sample_count;
+            if (sc == sc1) {
+                if (sc >= chart_max_len) { t_head = sc - chart_max_len; } else { t_head = 512 - chart_max_len + sc; }
+                this.draw_chart(t_head, chart_max_len);
+            }
+        }
+
+    }
+
+    rcv_data_input(d) {
+        var kk: Array<number> = Array.prototype.slice.call(new Uint8Array(d.buffer));
+        this.recv_buf = this.recv_buf.concat(kk);
+        while (1 == this.rcv_data_process()) { };
+    }
+
+    rcv_data_process() {
+        var len = this.recv_buf.length;
+        if (len < 16) return 0;//this.package_size
+        this.recv_total_pack_count++;
+        if (this.recv_buf[0] == 0x30 && this.recv_buf[15] == 'A'.charCodeAt(0)) {
+            switch (this.recv_buf[1]) {
+                case 'h'.charCodeAt(0):
+                    db_msg("\r\nhello");
+                    break;
+                case  'p'.charCodeAt(0):
+                    cpu_id = "";
+                    for (var i = 0; i < 12; i++)
+                    {
+                        cpu_id += this.recv_buf[2 + i].toString(16);
+                      //  s_pid1 += device_pid[i].ToString("X2") + "";
+                    }
+                    dev_info_has_read = true;
+                    break;
+                case  'q'.charCodeAt(0):
+                    version_info = "";
+                    for (var i = 0; i < 12; i++)
+                    {
+                        version_info += this.recv_buf[2 + i].toString(16);
+                    }
+                    break;
+                case 'e'.charCodeAt(0):
+                    var loc = 3;
+                    mcu_emissivity = this.recv_buf[loc] * 1 + this.recv_buf[loc + 1] * 0.1 + this.recv_buf[loc + 2] * 0.01 + this.recv_buf[loc + 3] * 0.001 + this.recv_buf[loc + 4] * 0.0001;
+                    break;
+                case 't'.charCodeAt(0):
+                    switch (process_state) {
+                        case 1:
+                            process_state = 2;
+                            //save data
+                            break;
+                        case 2:
+                            break;
+                        case 0:
+                            rt_obj = this.Cal_temp(2, this.recv_buf);
+                            rt_amb = this.Cal_temp(7, this.recv_buf);
+                            obj_list[this.sample_count] = rt_obj;
+                            amb_list[this.sample_count] = rt_amb;
+                            this.sample_count = (this.sample_count + 1) & 0x1ff;
+                            this.recv_data_pack_count++;
+
+                            this.obj_text_div.innerText = rt_obj.toFixed(2) + degree_str + " ";
+                            this.amb_text_div.innerHTML = "<br> <br>  @ " + rt_amb.toFixed(2) + degree_str;
+
+                            break;
+                    }
+                    
+                    break;
+                default:
+                    db_msg("E1");
+                    break;
+            }
+            this.recv_buf.splice(0, 16);
+        }
+        else {
+            this.recv_buf.splice(0, 1);
+        }
+
+        return 1;
+    }
+     Cal_temp( loc, data) {
+        return data[loc] * 100.0 + data[loc + 1] * 10.0 + data[loc + 2] + data[loc + 3] * 0.1 + data[loc + 4] * 0.01 - 273.15;
+    }
+
+
+    Cal_cent_Fah(c, is_centigrade) {
+        return is_centigrade ? c : (32 + c * 1.8);
+    }
+}
